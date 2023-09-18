01c80809075d960964315a5457035cf26689dae7
Bryan Quinn <bryan.quinn17@gmail.com>
2023-09-18 18:27:37 -0400

    Update roster and docker files

--- scraper/40_man_roster/web_scraper.py
+++ scraper/40_man_roster/web_scraper.py
@@ -176,6 +176,6 @@ def check_for_updates(new_player_json):
 
 
 if __name__ == '__main__':
-    # html_table = download_table()
-    html_table = load_table_from_file()
+    html_table = download_table()
+    # html_table = load_table_from_file()
     convert_table_to_json(html_table)
